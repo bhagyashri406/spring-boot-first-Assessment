6288cef4fcdd2d70e3490a0028b54ab1b0f381cb
bhagya <bhagyashri406@gmail.com>
2020-04-26 16:08:26 +0530

    added new changes

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into exchange_value (id, currency_from , currency_to ,conversion_multiple ,port) 
+values(1001, 'USD' , 'INR' , 57, 0);
+
+insert into exchange_value (id, currency_from , currency_to ,conversion_multiple,port) 
+values(1002, 'IND' , 'USD' , 75 ,0);
+
+insert into exchange_value (id, currency_from , currency_to ,conversion_multiple,port) 
+values(1003, 'EUR' , 'INR' , 87,0);
+
+insert into exchange_value (id, currency_from , currency_to ,conversion_multiple ,port) 
+values(1004, 'ISD' , 'EUR' , 37,0);
+
+

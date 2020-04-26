a5292282398a9ce72e3ff0b98ff8b5c0de49a940
bhagya <bhagyashri406@gmail.com>
2020-04-26 16:12:53 +0530

    changes

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/microservices/netflixzuulapigatewayserver/ZuulLogginFilter.java
@@ -0,0 +1,54 @@
+package com.microservices.netflixzuulapigatewayserver;
+
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.protocol.RequestContent;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+
+
+@Component
+public class ZuulLogginFilter extends ZuulFilter {
+	
+	
+	private org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass()) ;
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		
+		
+	HttpServletRequest request =	RequestContext.getCurrentContext().getRequest();
+		
+	logger.info("request -> {} request uri -> {}", request, request.getRequestURI());;
+	return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+}
